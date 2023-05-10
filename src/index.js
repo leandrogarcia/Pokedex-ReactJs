96b7775b0d07d034633f364c8ad047bb498d6216
Leandro Garcia <leandro.oliveira.garcia@gmail.com>
2023-05-10 20:23:02 -0300

    adicionanod routerdom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import Home from './pages/home';
+import RoutesApp from './routes';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <RoutesApp/>
   </React.StrictMode>
 );
 
